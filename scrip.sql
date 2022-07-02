d29cca75fc382f79d155e7c6e071642efe5d91dc
Santiago Benitez <sbenitez1607@gmail.com>
2022-07-01 22:36:03 -0500

    se agregan los modelos y el scrip para generar las tablas

---
+++ scrip.sql
@@ -0,0 +1,51 @@
+/*==============================================================*/
+/* DBMS name:      ORACLE Version 11g                           */
+/* Created on:     1/07/2022 10:17:54 p. m.                     */
+/*==============================================================*/
+
+
+alter table TBLSUCURSAL
+   drop constraint FK_TBLSUCUR_TIENE_TBLBANCO;
+
+drop table TBLBANCO cascade constraints;
+
+drop index TIENE_FK;
+
+drop table TBLSUCURSAL cascade constraints;
+
+/*==============================================================*/
+/* Table: TBLBANCO                                              */
+/*==============================================================*/
+create table TBLBANCO 
+(
+   NITBANCO             NUMBER(15,0)         not null,
+   NOMBREBANCO          VARCHAR2(50)         not null,
+   FECHAFUNDACIONBANCO  DATE                 not null,
+   constraint PK_TBLBANCO primary key (NITBANCO)
+);
+
+/*==============================================================*/
+/* Table: TBLSUCURSAL                                           */
+/*==============================================================*/
+create table TBLSUCURSAL 
+(
+   CODSUCURSAL          NUMBER(15,0)         not null,
+   NITBANCO             NUMBER(15,0)         not null,
+   UBICACIONSUCURSAL    VARCHAR2(10)         not null
+      constraint CKC_UBICACIONSUCURSAL_TBLSUCUR check (UBICACIONSUCURSAL in ('norte','sur','centro')),
+   NUMEMPLEADOSSUCURSAL NUMBER(7,0)          not null,
+   FECHAFUNDACIONSUCURSAL DATE                 not null,
+   constraint PK_TBLSUCURSAL primary key (CODSUCURSAL)
+);
+
+/*==============================================================*/
+/* Index: TIENE_FK                                              */
+/*==============================================================*/
+create index TIENE_FK on TBLSUCURSAL (
+   NITBANCO ASC
+);
+
+alter table TBLSUCURSAL
+   add constraint FK_TBLSUCUR_TIENE_TBLBANCO foreign key (NITBANCO)
+      references TBLBANCO (NITBANCO);
+
